3b5d80f8cb4adf4ff97d2a7b206bd1530f290053
Austin M. Reppert <austinmreppert@protonmail.com>
2022-07-06 12:09:26 -0400

    Added new route.

---
+++ pages/api/timezonemap.ts
@@ -0,0 +1,47 @@
+import { time } from 'console';
+import { DateTime } from 'luxon';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type TimeMap = {
+  key: string,
+  value: string
+}
+
+type Response = {
+  times: Array<TimeMap>
+}
+
+type Error = {
+  message: string
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Response | Error>) {
+
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not supported." })
+    return;
+  }
+
+  const outputTimeZone: string = req.body.outputTimeZone;
+  const outputFormat: string = req.body.outputFormat;
+  const times: Array<string> = req.body.times;
+
+  let response: Response = {
+    times: []
+  };
+
+  let map = new Map<string, string>();
+
+  times.forEach((time: string) => {
+    map.set(time, DateTime.fromISO(time).setZone(outputTimeZone).toFormat(outputFormat).toString());
+  });
+
+  map.forEach((key: string, value: string) => {
+    response.times.push({
+      key: key,
+      value: value
+    });
+  });
+
+  res.status(200).json(response);
+}
